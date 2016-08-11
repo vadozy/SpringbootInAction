350fb7a7e507cab1b411816226e3db35555a946d
Vadim Storozhuk <Vadim.Storozhuk@numeric.com>
2016-08-11 18:05:12 -0400

    Wip

--- src/main/java/com/manning/controller/ReadingListController.java
+++ src/main/java/com/manning/controller/ReadingListController.java
@@ -3,6 +3,7 @@ package com.manning.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.manning.springdata.ReadingListRepository;
  */
 @Controller
 @RequestMapping("/")
+//@ConfigurationProperties("amazon")
 public class ReadingListController {
 
 	private ReadingListRepository readingListRepository;
