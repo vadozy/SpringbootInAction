aeae7e4861d64be75ed4730136d6fa7bc402b706
Vadim Storozhuk <vadim.storo@gmail.com>
2016-08-13 15:30:18 -0400

    Ch04 Wip. Controller tests for Get and Post requests are written.

--- src/test/java/com/manning/MockMvcWebTests.java
+++ src/test/java/com/manning/MockMvcWebTests.java
@@ -1,12 +1,21 @@
 package com.manning;
 
+import static org.hamcrest.Matchers.*;
+
+import com.manning.domain.Book;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -18,17 +27,55 @@ import org.springframework.web.context.WebApplicationContext;
 @WebAppConfiguration
 public class MockMvcWebTests {
 
-	@Autowired
-	private WebApplicationContext webContext;
+    @Autowired
+    private WebApplicationContext webContext;
 
-	private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-	@Before
-	public void setupMockMvc() {
-		mockMvc = MockMvcBuilders
-				.webAppContextSetup(webContext)
-				//.apply(springSecurity())
-				.build();
-	}
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webContext)
+                //.apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void homePage() throws Exception {
+        mockMvc.perform(get("/readingList/vadozy"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books",
+                        is(empty())));
+    }
+
+    @Test
+    public void postBook() throws Exception {
+        mockMvc.perform(post("/readingList/vadozy")
+                .contentType(APPLICATION_FORM_URLENCODED)
+                .param("title", "BOOK TITLE")
+                .param("author", "BOOK AUTHOR")
+                .param("isbn", "1234567890")
+                .param("description", "DESCRIPTION"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "vadozy"));
+
+        Book expectedBook = new Book();
+        expectedBook.setId(1L);
+        expectedBook.setReader("vadozy");
+        expectedBook.setTitle("BOOK TITLE");
+        expectedBook.setAuthor("BOOK AUTHOR");
+        expectedBook.setIsbn("1234567890");
+        expectedBook.setDescription("DESCRIPTION");
+
+        mockMvc.perform(get("/readingList/vadozy"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", hasSize(1)))
+                .andExpect(model().attribute("books",
+                        contains(samePropertyValuesAs(expectedBook))));
+    }
 
 }
